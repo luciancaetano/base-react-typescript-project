7ae7f82c15929173be28901636508b3a70655a34
LucianCaetano <lucian.santos.caetano@gmail.com>
2024-01-29 19:44:21 -0300

    chore: :rotating_light: updasted lint rules

--- src/lib/tests/index.tsx
+++ src/lib/tests/index.tsx
@@ -1,17 +1,29 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable react/display-name */
 import AppProvider from '@components/providers/app-provider';
 import { render as testRender } from '@testing-library/react';
 import { renderHook as testRenderHook, act } from '@testing-library/react-hooks';
-import React from 'react';
+import React, { ComponentType } from 'react';
 
-const wrapper = ({ children }: React.PropsWithChildren<object>) => (
+export const appWrapper = ({ children }: React.PropsWithChildren<object>) => (
   <AppProvider>
     {children}
   </AppProvider>
 );
 
-export const render = (Component: React.ReactElement) => testRender(Component, { wrapper });
+export const render = (Component: React.ReactElement) => testRender(Component, { wrapper:appWrapper });
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const renderHook: typeof testRenderHook = (callback, options) => testRenderHook(callback, { ...options, wrapper: wrapper as any });
+export const renderHook: typeof testRenderHook = (callback, options) => testRenderHook(callback, { wrapper: appWrapper as any, ...options });
 
 export { act };
+
+export function withAppWrapper<T>(
+  WrappedComponent: ComponentType<T>,
+): ComponentType<T> {
+  return (props: T) => (
+    <AppProvider>
+      <WrappedComponent {...props as any} />
+    </AppProvider>
+  );
+}
