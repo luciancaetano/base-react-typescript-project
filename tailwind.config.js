ddc1ba0acf3837f0ba382ac1d36b76c56fbdcedd
Lucian Caetano <lucian.santos.caetano@gmail.com>
2025-01-20 14:04:32 -0300

    feat: :sparkles: introduced some basic feature configs and tailwind, addded some things related to session and tenants to saas apps

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,61 +22,6 @@ module.exports = {
     extend: {
       colors: {
         ...colors,
-        dark: '#1d232a',
-        primary: {
-          50: '#eaf5ff',
-          100: '#d6eaff',
-          200: '#add5ff',
-          300: '#84c1ff',
-          400: '#5badff',
-          500: '#3b9bf3',
-          DEFAULT: '#3b9bf3',
-          600: '#2e7bcc',
-          700: '#225ca5',
-          800: '#173d7e',
-          900: '#0c1f57',
-        },
-        sidebar: {
-          base: "#f9fafb",
-          divider: "#ababab",
-          "clinic-box": "#EFEFF1",
-          "clinic-box-text": "#50555D",
-          "clinic-box-text-addr": "#99A0B2",
-          item: {
-            default: {
-              content: "#676769",
-            },
-            active: {
-              content: "#3b9bf3ff",
-              bg: "#E8EFFB",
-            },
-            hover: {
-              content: "#3b9bf3ff",
-              bg: "#c8daf7",
-            },
-          },
-        },
-        "sidebar-dark": {
-          bg: "rgb(26 29 33)",
-          base: "#f9fafb",
-          divider: "#ababab",
-          "clinic-box": "#c1c1c1",
-          "clinic-box-text": "#c1c1c1",
-          "clinic-box-text-addr": "#c1c1c1",
-          item: {
-            default: {
-              content: "#c1c1c1",
-            },
-            active: {
-              content: "#c1c1c1",
-              bg: "#343d49",
-            },
-            hover: {
-              content: "#c1c1c1",
-              bg: "#343d49",
-            },
-          },
-        },
       },
       fontSize: {
         ssm: "0.75rem",
